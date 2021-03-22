5689f6969e523d3ef08b22e47cc7c09e713b466f
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-03-22 13:21:02 +0200

    Added a test controller for login and authorize

---
+++ EarlyBird.API/Controllers/TestController.cs
@@ -0,0 +1,40 @@
+﻿using EarlyBird.API.Models;
+using EarlyBird.BusinessLogic.Services;
+using EarlyBird.BusinessLogic.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EarlyBird.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IUsersService usersService;
+
+        public TestController(IUsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginRequest credentials)
+        {
+            var token = usersService.Authenticate(credentials.Username, credentials.Password);
+            if (token == null) return Unauthorized();
+            return Ok(new LoginResponse { Token = token });
+        }
+
+        [HttpGet]
+        [Authorize(Policy = Constants.Policies.Admin)]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ EarlyBird.API/Models/LoginResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EarlyBird.API.Models
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+    }
+}
