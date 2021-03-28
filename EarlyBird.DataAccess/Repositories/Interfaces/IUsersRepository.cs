8ce16390020c5697156e2b790d16328fa95e5900
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-03-28 20:14:06 +0300

    deleted unused using statements

--- EarlyBird.DataAccess/Repositories/Interfaces/IUsersRepository.cs
+++ EarlyBird.DataAccess/Repositories/Interfaces/IUsersRepository.cs
@@ -1,9 +1,5 @@
 ﻿using EarlyBird.DataAccess.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EarlyBird.DataAccess.Repositories.Interfaces
 {
