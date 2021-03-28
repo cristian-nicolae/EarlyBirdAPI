53244396c14fb4ddfa5ab6275c815f954c581000
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-03-28 21:55:56 +0300

    added repo files

---
+++ EarlyBird.DataAccess/Repositories/CategoriesRepository.cs
@@ -0,0 +1,43 @@
+﻿using EarlyBird.DataAccess.Entities;
+using EarlyBird.DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarlyBird.DataAccess.Repositories
+{
+    public class CategoriesRepository : ICategoriesRepository
+    {
+        public CategoryEntity Add(CategoryEntity categoryEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CategoryEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CategoryEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CategoryEntity GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CategoryEntity categoryEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EarlyBird.DataAccess/Repositories/OffersRepository.cs
@@ -0,0 +1,43 @@
+﻿using EarlyBird.DataAccess.Entities;
+using EarlyBird.DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarlyBird.DataAccess.Repositories
+{
+    public class OffersRepository : IOffersRepository
+    {
+        public OfferEntity Add(OfferEntity offerEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<OfferEntity> GetAllAvailableOffers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public OfferEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<OfferEntity> GetPublisherOffers(Guid publisherId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(OfferEntity offerEntity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
