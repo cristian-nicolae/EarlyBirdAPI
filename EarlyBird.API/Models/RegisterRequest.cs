83a4b04d1c08cd883f4721859b28fbaede0ec99b
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-02 22:19:58 +0300

    implemented register controller

---
+++ EarlyBird.API/Models/RegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿using EarlyBird.DataAccess.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EarlyBird.API.Models
+{
+    public class RegisterRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Roles Role { get; set; }
+    }
+}
