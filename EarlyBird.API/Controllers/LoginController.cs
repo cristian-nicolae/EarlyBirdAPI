88dc2e2bb281dfce9490872541bf2b6a320ceb2a
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-02 22:09:36 +0300

    made login controller

---
+++ EarlyBird.API/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using EarlyBird.API.Models;
+using EarlyBird.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EarlyBird.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly UsersService usersService;
+
+        public LoginController(UsersService usersService)
+        {
+            this.usersService = usersService;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginRequest credentials)
+        {
+            var token = usersService.Authenticate(credentials.Username, credentials.Password);
+            if (token == null) return Forbid();
+            return Ok(new LoginResponse { Token = token });
+        }
+    }
+}
