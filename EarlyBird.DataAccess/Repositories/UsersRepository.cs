8795967ae0b63ebf689fc9092177efe67dd93e39
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-03 12:30:43 +0300

    Create UsersRepository.cs

---
+++ EarlyBird.DataAccess/Repositories/UsersRepository.cs
@@ -0,0 +1,44 @@
+﻿using EarlyBird.DataAccess.Entities;
+using EarlyBird.DataAccess.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarlyBird.DataAccess.Repositories
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly EarlyBirdContext context;
+
+        public UsersRepository(EarlyBirdContext context)
+        {
+            this.context = context;
+        }
+        public UserEntity Add(UserEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(UserEntity user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserEntity GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserEntity GetByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
