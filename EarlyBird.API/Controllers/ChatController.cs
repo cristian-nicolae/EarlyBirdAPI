6e5e9d5f3a0781bb94fd075c0f8428ca7f9d90c3
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-30 19:56:20 +0300

    send user

--- EarlyBird.API/Controllers/ChatController.cs
+++ EarlyBird.API/Controllers/ChatController.cs
@@ -1,10 +1,14 @@
 ﻿using EarlyBird.API.Hubs;
 using EarlyBird.API.Models;
 using EarlyBird.API.Utils;
+using EarlyBird.BusinessLogic.DTOs;
+using EarlyBird.BusinessLogic.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace EarlyBird.API.Controllers
@@ -14,19 +18,34 @@ namespace EarlyBird.API.Controllers
     public class ChatController : ControllerBase
     {
         private readonly IHubContext<ChatHub> _chatHub;
+        private readonly ITokenService _tokenService;
+        private readonly IUsersService _usersService;
 
-        public ChatController(IHubContext<ChatHub> chatHub)
+        public ChatController(IHubContext<ChatHub> chatHub, 
+                                ITokenService tokenService, 
+                                IUsersService usersService)
         {
             _chatHub = chatHub;
+            _tokenService = tokenService;
+            _usersService = usersService;
         }
 
         [HttpPost("messages")]
         [Authorize]
         public async Task Post(ChatMessage message)
         {
-            // run some logic...
-
+            var user = GetCurrentUser();
+            message.User = user.Firstname;
             await _chatHub.Clients.All.SendAsync("ReceiveMessage", message);
         }
+
+        #region private methods
+
+        private ViewUserDto GetCurrentUser()
+        {
+            var userId = _tokenService.GetUserIdFromClaims(HttpContext.User.Identity as ClaimsIdentity);
+            return _usersService.GetById(userId);
+        }
+        #endregion
     }
 }
