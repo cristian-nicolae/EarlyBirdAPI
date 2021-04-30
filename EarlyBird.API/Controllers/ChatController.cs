14fee85c32d3f8c292cab672b0313e8cd0568f4a
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-30 19:32:51 +0300

    add chat basic functionality

---
+++ EarlyBird.API/Controllers/ChatController.cs
@@ -0,0 +1,32 @@
+﻿using EarlyBird.API.Hubs;
+using EarlyBird.API.Models;
+using EarlyBird.API.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EarlyBird.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> _chatHub;
+
+        public ChatController(IHubContext<ChatHub> chatHub)
+        {
+            _chatHub = chatHub;
+        }
+
+        [HttpPost("messages")]
+        [Authorize]
+        public async Task Post(ChatMessage message)
+        {
+            // run some logic...
+
+            await _chatHub.Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
