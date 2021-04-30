e6086c0b2fbab0a8fa122cc64a5f2a58cdee07aa
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-04-30 23:09:49 +0300

    save connected users

---
+++ EarlyBird.BusinessLogic/Services/ConnectedUsersService.cs
@@ -0,0 +1,28 @@
+﻿using EarlyBird.BusinessLogic.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace EarlyBird.BusinessLogic.Services
+{
+    public class ConnectedUsersService: IConnectedUsersService
+    {
+        private Dictionary<Guid, string> ConnectedUsers = new Dictionary<Guid, string>();
+        
+        public void Add(Guid userId, string connectionId)
+        {
+            ConnectedUsers.Add(userId, connectionId);
+        }
+
+        public void Remove(Guid userId)
+        {
+            ConnectedUsers.Remove(userId);
+        }
+
+        public string GetConnectionId(Guid userId)
+        {
+            if (!ConnectedUsers.ContainsKey(userId))
+                return null;
+            return ConnectedUsers.GetValueOrDefault(userId);
+        }
+    }
+}
---
+++ EarlyBird.BusinessLogic/Services/Interfaces/IConnectedUsersService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EarlyBird.BusinessLogic.Services.Interfaces
+{
+    interface IConnectedUsersService
+    {
+        public void Add(Guid userId, string connectionId);
+        public void Remove(Guid userId);
+        public string GetConnectionId(Guid userId);
+    }
+}
