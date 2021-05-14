03b585a084a20d82cd8d6e589a8a373f682ffcb2
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-05-14 17:12:08 +0300

    add categories to offers

---
+++ EarlyBird.DataAccess/Repositories/Interfaces/IOfferCategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace EarlyBird.DataAccess.Repositories.Interfaces
+{
+    public interface IOfferCategoryRepository
+    {
+        bool AddRange(IEnumerable<int> categoryIds, int offerId);
+    }
+}
